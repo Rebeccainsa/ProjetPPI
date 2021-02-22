3586a5e37360c2be930fb14c421db606eebc0821
Rebeccainsa <rebeccavo98@gmail.com>
2021-02-22 17:53:43 +0100

    séparation js et css et ajout texte
    séparation du js et du css dans deux autres pages. Ajout des vrais paragraphes de texte et mise en forme de ce dernier (trace pour l'instant).  Appartition des textes en overflow

---
+++ script.js
@@ -0,0 +1,157 @@
+		//afficher les informations sur les experts en empreinte
+function affichertrace() {
+  if (document.getElementById("trace").style.display == "block") {
+      document.getElementById("trace").style.display = "none";
+  }
+  else {
+      document.getElementById("trace").style.display = "block";
+  }
+}
+
+function affichertrace2() {
+  document.getElementById("canvtrace").style.border = "1px solid black";
+}
+
+function affichertrace3() {
+  document.getElementById("canvtrace").style.border = "none";
+}
+
+
+
+//afficher informations expert ADN
+function afficheradn() {
+  if (document.getElementById("ADN").style.display == "block") {
+      document.getElementById("ADN").style.display = "none";
+  }
+  else {
+      document.getElementById("ADN").style.display = "block";
+  }
+}
+
+function afficheradn2() {
+  document.getElementById("canvADN").style.border = "1px solid black";
+}
+
+function afficheradn3() {
+  document.getElementById("canvADN").style.border = "none";
+}
+
+
+
+//afficher informations experts véhicule  
+function affichervehicule() {
+  if (document.getElementById("véhicule").style.display == "block") {
+      document.getElementById("véhicule").style.display = "none";
+  }
+  else {
+      document.getElementById("véhicule").style.display = "block";
+  }
+}
+
+function affichervehicule2() {
+  document.getElementById("canvvehicule").style.border = "1px solid black";
+}
+
+function affichervehicule3() {
+  document.getElementById("canvvehicule").style.border = "none";
+}
+
+
+
+//afficher informations experts signal
+function affichersignal() {
+  if (document.getElementById("signal").style.display == "block") {
+      document.getElementById("signal").style.display = "none";
+  }
+  else {
+      document.getElementById("signal").style.display = "block";
+  }
+}
+
+function affichersignal2() {
+  document.getElementById("canvsignal").style.border = "1px solid black";
+}
+
+function affichersignal3() {
+  document.getElementById("canvsignal").style.border = "none";
+}
+
+
+
+//afficher informations experts numérique
+function affichernumerique() {
+  if (document.getElementById("numérique").style.display == "block") {
+      document.getElementById("numérique").style.display = "none";
+  }
+  else {
+      document.getElementById("numérique").style.display = "block";
+  }
+}
+
+function affichernumerique2() {
+  document.getElementById("canvnum").style.border = "1px solid black";
+}
+
+function affichernumerique3() {
+  document.getElementById("canvnum").style.border = "none";
+}
+
+
+
+//afficher informations experts manuscrit 
+function affichermanuscrit() {
+  if (document.getElementById("manuscrit").style.display == "block") {
+      document.getElementById("manuscrit").style.display = "none";
+  }
+  else {
+      document.getElementById("manuscrit").style.display = "block";
+  }
+}
+
+function affichermanuscrit2() {
+  document.getElementById("canvman").style.border = "1px solid black";
+}
+
+function affichermanuscrit3() {
+  document.getElementById("canvman").style.border = "none";
+}
+
+
+
+//afficher informations experts ballistique 
+function afficherballe() {
+  if (document.getElementById("ballistique").style.display == "block") {
+      document.getElementById("ballistique").style.display = "none";
+  }
+  else {
+      document.getElementById("ballistique").style.display = "block";
+  }
+}
+
+function afficherballe2() {
+  document.getElementById("canvballe").style.border = "1px solid black";
+}
+
+function afficherballe3() {
+  document.getElementById("canvballe").style.border = "none";
+}
+
+
+
+//afficher informations experts toxicologie 
+function affichertoxico() {
+  if (document.getElementById("toxico").style.display == "block") {
+      document.getElementById("toxico").style.display = "none";
+  }
+  else {
+      document.getElementById("toxico").style.display = "block";
+  }
+}
+
+function affichertoxico2() {
+  document.getElementById("canvtoxico").style.border = "1px solid black";
+}
+
+function affichertoxico3() {
+  document.getElementById("canvtoxico").style.border = "none";
+}
